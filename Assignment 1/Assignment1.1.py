4e9c98ad1e60eb7220c2494e11930e0a2c269098
HarrieO <harrieoosterhuis@gmail.com>
2014-10-29 14:53:22 +0100

    assignment 1

---
+++ Assignment 1/Assignment1.1.py
@@ -0,0 +1,12 @@
+from world    import World
+from prey     import Prey
+from predator import Predator
+import numpy as np
+		
+runs = np.array([0]*100)
+for i in range(100):
+	runs[i] = World(Prey(0,0), Predator(5,5)).run();
+
+print "100 runs completed"
+print "Average number of steps per run: ", np.sum(runs)/100
+print "Standard Deviation: ", np.std(runs)

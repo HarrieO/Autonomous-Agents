1f39498ee4f7ff550f461fc5e9a192d570fc002a
Carla Groenland <carla.groenland@gmail.com>
2014-10-29 14:07:12 +0100

    no message

--- Assignment 1/predator.py
+++ Assignment 1/predator.py
@@ -12,10 +12,16 @@ class Predator(object):
 		randNum = np.floor(np.random.rand()*5)
 		if(randNum ==0):
 			self.x = (self.x +1)%11
+			return (1,0)
 		elif(randNum==1):
 			self.x = (self.x -1)%11
+			return (-1,0)
 		elif(randNum==2):
 			self.x = (self.y +1)%11
+			return (0,1)
 		elif(randNum==3):
 			self.x = (self.y -1)%11
+			return (0,-1)
+		else:
+			return (0,0)
 
--- Assignment 1/prey.py
+++ Assignment 1/prey.py
@@ -12,9 +12,15 @@ class Prey(object):
 		randNum = np.floor(np.random.rand()*10)
 		if(randNum ==0):
 			self.x = (self.x +1)%11
+			return (1,0)
 		elif(randNum==1):
 			self.x = (self.x -1)%11
+			return (-1,0)
 		elif(randNum==2):
 			self.x = (self.y +1)%11
+			return (0,1)
 		elif(randNum==3):
 			self.x = (self.y -1)%11
+			return (0,-1)
+		else:
+			return (0,0)

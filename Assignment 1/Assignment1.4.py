2277953a04180b5b07ec92303b130a94d982db11
Carla Groenland <carla.groenland@gmail.com>
2014-10-29 16:31:13 +0100

    changes to assignment 1.4

--- Assignment 1/Assignment1.4.py
+++ Assignment 1/Assignment1.4.py
@@ -5,35 +5,49 @@ import numpy as np
 
 values = np.zeros([11,11])
 
-
 def reward(predator, move, prey):
-	if predator.x+move[0] == prey.x and predator.y+move[1] == prey.y:
+	if (predator[0]+move[0])%11 == prey.x and (predator[1]+move[1])%11 == prey.y:
 		return 10
 	return 0
 
-moves = [(0,0),(0,1),(0,-1),(1,0),(-1,0)]
-predator = Predator()
+def valueIteration(prey):
+	moves = [(0,0),(0,1),(0,-1),(1,0),(-1,0)]
+	discountFactor = 0.8
 
-discountFactor = 0.8
-
-epsilon = 0.01
-delta = 2.0*epsilon
-while delta >= epsilon:
+	epsilon = 0.01
+	delta = 0.0
 	oldValues = values.copy()
-	for y in range(11):
-		for x in range(11):
+	for y in range(10):
+		for x in range(10):
 			sumList = np.zeros(len(moves))
 			for i in range(len(moves)):
 				move = moves[i]
-				sumList[i] =reward(predator, move, prey) + discountFactor*oldValues[x+move[0],y+move[1]]
+				sumList[i] = reward([x,y], move, prey) + discountFactor*oldValues[x+move[0],y+move[1]]
 			maximum =  max(sumList)
 			delta = max(delta,np.abs(values[x,y]-maximum))
 			values[x,y] = maximum
 
-def policy(state):
-	sumList = np.zeros(len(moves))
-	for i in range(len(moves)):
-		move = moves[i]
-		sumList[i] =reward(predator, move, prey) + discountFactor*values[x+move[0],y+move[1]]
-	bestIndex = np.argmax(sumList)
-	return moves[bestIndex]
+	while delta >= epsilon:
+		oldValues = values.copy()
+		for y in range(10):
+			for x in range(10):
+				sumList = np.zeros(len(moves))
+				for i in range(len(moves)):
+					move = moves[i]
+					sumList[i] = reward([x,y], move, prey) + discountFactor*oldValues[x+move[0],y+move[1]]
+				maximum =  max(sumList)
+				delta = max(delta,np.abs(values[x,y]-maximum))
+				values[x,y] = maximum
+
+	def policy(state):
+		sumList = np.zeros(len(moves))
+		for i in range(len(moves)):
+			move = moves[i]
+			sumList[i] = reward(predator, move, prey) + discountFactor*values[state[0]+move[0],state[1]+move[1]]
+		bestIndex = np.argmax(sumList)
+		return moves[bestIndex]
+
+	return policy
+prey = Prey(5,5)
+p = valueIteration(prey)
+print p([0,0])

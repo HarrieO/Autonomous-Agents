b47b98b8c409c5c33ac9fef66c224bb50fb62f3b
HarrieO <harrieoosterhuis@gmail.com>
2014-10-29 16:30:31 +0100

    call it a day

--- Assignment 1/Assignment1.2.py
+++ Assignment 1/Assignment1.2.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def reward(predator, move, prey):
-	if (predator[0]+move[0])%11 == prey.x and (predator[1]+move[1])%11 == prey.y:
+	if (predator[0]+move[0])%11 == prey[0] and (predator[1]+move[1])%11 == prey[1]:
 		return 10
 	return 0
 
@@ -13,16 +13,14 @@ def reward(predator, move, prey):
 def valueGrid(prey):
 	values = [ [ 0 for y in range(11) ] for x in range(11)]
 
-	epsilon = 0.001
+	epsilon = 0.1
 	delta = epsilon*2
 
 	moves = [(0,0),(0,1),(0,-1),(1,0),(-1,0)]
 	probs  = [0.2, 0.2, 0.2, 0.2, 0.2]
 
-	predator = Predator(5,5)
-	prey = Prey(0,0)
-
 	discountFactor = 0.8
+	deltas = []
 
 	while delta > epsilon:
 		delta = 0
@@ -31,12 +29,17 @@ def valueGrid(prey):
 				temp = values[x][y]
 				moveSum = 0
 				for move, prob in zip(moves,probs):
-					moveSum += prob * (reward((x,y), move, prey) + discountFactor*values[x][y])
+					nx, ny = (x + move[0])%11, (y + move[1])%11 
+					moveSum += prob * (reward((x,y), move, prey) + discountFactor*values[nx][ny])
 				values[x][y] = moveSum
 				delta = max(delta, values[x][y] - temp)
+	return values, deltas
 
-	return values
+predators = [(0,0),(2,3),(2,10),(10,10)]
+preys     = [(5,5),(5,4),(10,0),(0,0)]
 
-print valueGrid((5,5))
+
+for predator, prey in zip(predators,preys):
+	print valueGrid(prey)[predator[0]][predator[1]]
 
 

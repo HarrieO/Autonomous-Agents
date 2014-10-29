1b525199c3c54adf769becf2f4f1ac61559b76ff
HarrieO <harrieoosterhuis@gmail.com>
2014-10-29 15:56:16 +0100

    assigment 1.2 almost finished

--- Assignment 1/Assignment1.2.py
+++ Assignment 1/Assignment1.2.py
@@ -5,29 +5,38 @@ import numpy as np
 
 
 def reward(predator, move, prey):
-	if predator.x+move[0] == prey.x and predator.y+move[1] == prey.y:
+	if (predator[0]+move[0])%11 == prey.x and (predator[1]+move[1])%11 == prey.y:
 		return 10
 	return 0
 
-values = [ [ 0 for y in range(11) ] for x in range(11)]
 
-epsilon = 0.1
-delta = epsilon*2
+def valueGrid(prey):
+	values = [ [ 0 for y in range(11) ] for x in range(11)]
 
-moves = [(0,0),(0,1),(0,-1),(1,0),(-1,0)]
-prob  = [0.2, 0.2, 0.2, 0.2, 0.2]
+	epsilon = 0.001
+	delta = epsilon*2
 
-predator = Predator()
+	moves = [(0,0),(0,1),(0,-1),(1,0),(-1,0)]
+	probs  = [0.2, 0.2, 0.2, 0.2, 0.2]
 
-discountFactor = 0.8
+	predator = Predator(5,5)
+	prey = Prey(0,0)
 
-while delta > epsilon:
-	valuesOld = [row[:] for row in values]
-	for y in range(11):
-		for x in range(11):
-			temp = values[x][y]
-			moveSum = 0
-			for move, prob in moves:
-				moveSum += prob * ()
+	discountFactor = 0.8
+
+	while delta > epsilon:
+		delta = 0
+		for y in range(11):
+			for x in range(11):
+				temp = values[x][y]
+				moveSum = 0
+				for move, prob in zip(moves,probs):
+					moveSum += prob * (reward((x,y), move, prey) + discountFactor*values[x][y])
+				values[x][y] = moveSum
+				delta = max(delta, values[x][y] - temp)
+
+	return values
+
+print valueGrid((5,5))
 
 

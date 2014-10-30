b210516db0056e23841b45c91cf57edf70c1668f
Carla Groenland <carla.groenland@gmail.com>
2014-10-30 11:00:51 +0100

    finished code 1.3

--- Assignment 1/Assignment1.3.py
+++ Assignment 1/Assignment1.3.py
@@ -3,14 +3,8 @@ from prey     import Prey
 from agent 	  import Agent
 import numpy as np
 
-# initialize policy
-policy = {}
-alllocations = [ (x,y) for x in range(11) for y in range(11)]
-for predloc in alllocations:
-	for preyloc in alllocations:
-		if preyloc != predloc:
-			policy[(predloc,preyloc)] = (0,0)
-
+discountFactor = 0.8
+epsilon = 0.01
 
 def evaluatePolicy(policy, values=None):
 	alllocations = [ (x,y) for x in range(11) for y in range(11)]
@@ -20,9 +14,6 @@ def evaluatePolicy(policy, values=None):
 		for preyloc in alllocations:
 			if preyloc != predloc:
 				values[(predloc,preyloc)] = 0
-	deltas = []
-	discountFactor = 0.8
-	epsilon = 0.1
 	delta = 1
 	while delta > epsilon:
 		delta = 0
@@ -42,20 +33,21 @@ def evaluatePolicy(policy, values=None):
 							preySum += preyProb * discountFactor * values[(newPredloc,newPreyloc)]
 				values[(predloc,preyloc)] = preySum
 				delta = max(delta, np.abs(preySum - temp))
-		deltas.append(delta)
-
 	return values
 
 def improvePolicy(policy,values):
-	stable = true
+	stable = True
 	alllocations = [ (x,y) for x in range(11) for y in range(11)]
 	moves = [(0,0),(0,1),(0,-1),(1,0),(-1,0)]
 	for predloc in alllocations:
 		for preyloc in alllocations:
+			if predloc == preyloc:
+				continue
+			prey 	 = Prey(*preyloc)
 			oldPolicy = policy[(predloc,preyloc)]
 			bestPolicy = (0,0)
 			bestVal = 0
-			for predMove in moves
+			for predMove in moves:
 				newPredloc = ((predloc[0] + predMove[0])%11,(predloc[1] + predMove[1])%11)
 				preySum = 0
 				if newPredloc == preyloc :
@@ -68,5 +60,19 @@ def improvePolicy(policy,values):
 					bestPolicy = predMove
 			policy[(predloc,preyloc)]=bestPolicy
 			if oldPolicy != bestPolicy:
-				stable = false
+				stable = False
 	return policy, stable
+
+# initialize policy
+policy = {}
+alllocations = [ (x,y) for x in range(11) for y in range(11)]
+for predloc in alllocations:
+	for preyloc in alllocations:
+		policy[(predloc,preyloc)] = (0,0)
+stable = False
+values = None
+while not stable:
+	values = evaluatePolicy(policy, values)
+	print "Evaluated values"
+	policy, stable = improvePolicy(policy, values)
+	print "Improved policy"

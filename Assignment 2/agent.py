e124f66f99c7de9e4475dc74d5cc7a7a76195e7b
fabba <fab_v_cool@hotmail.com>
2014-11-05 13:10:56 +0100

    Assignment 2

---
+++ Assignment 2/agent.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class Agent(object):
+	def __init__(self, x_init, y_init, name="Agent"):
+		self.x    = x_init
+		self.y 	  = y_init
+		self.name = name
+
+	def loc(self):
+		return (self.x,self.y)
+
+	def printState(self):
+		print self.name + "("+ str(self.x) + "," + str(self.y) + ")"
+
+	def sameLocation(self, location):
+		return self.x == location[0] and self.y == location[1]
+
+	def locAfterMove(self, move):
+		return ((self.x + move[0])%11, (self.y + move[1])%11)
+
+	def getMoveList(self, stateInfo=None):
+		moveList = [(0,0),(0,1),(0,-1),(1,0),(-1,0)]
+		prob 	 = 1.0/len(moveList)
+		return [(prob,move) for move in moveList ]
+
+	def expand(self, stateInfo=None):
+		states = []
+		for prob, move in self.getMoveList(stateInfo):
+			states.append((prob,self.locAfterMove(move)))
+		return states
+
+	def setLocation(self, location):
+		self.x, self.y = location
+
+	def move(self, move):
+		self.x, self.y = self.locAfterMove(move)
+
+	def pickMove(self, stateInfo=None):
+		moveList = self.getMoveList(stateInfo)
+		pick = np.random.rand()
+		for prob, move in moveList:
+			if pick <= prob:
+				return move
+			else:
+				pick -= prob
---
+++ Assignment 2/world.py
@@ -0,0 +1,58 @@
+from agent import *
+from prey import *
+
+class World(object):
+	def __init__(self, prey, predator, width=11, height=11):
+		self.width  	= width
+		self.height 	= height
+		#self.grid   	= [[None for x in range(width)] for y in range(height) ]
+		self.prey   	= prey
+		self.predator 	= predator
+
+	def prettyPrint(self, worldPrint, printStates):
+		if worldPrint:
+			print "#" * (self.width*5-1 + 4)
+			for y in range(self.height):
+				print "#",
+				for x in range(self.width):
+					if x == self.prey.x and y == self.prey.y:
+						print "PREY",
+					elif x == self.predator.x and y == self.predator.y:
+						print "PRED",
+					else:
+						print "____",
+				print "#"
+			print "#" * (self.width*5-1 + 4)
+			print
+		if printStates:
+			self.predator.printState()
+			self.prey.printState()
+			print
+
+
+	def stopState(self, move):
+		if self.prey.sameLocation(self.predator.locAfterMove(move)):
+			return True
+		return False
+
+	def run(self, worldPrint=False, printStates=False):
+		
+		self.prettyPrint(worldPrint,printStates)
+
+		iterations = 0;
+		
+		predMove = self.predator.pickMove()
+		while not self.stopState(predMove):
+			self.predator.move(predMove)
+			self.prey.move(self.prey.pickMove(self.predator.loc()))
+			predMove = self.predator.pickMove()
+
+			self.prettyPrint(worldPrint,printStates)
+
+			iterations += 1
+
+		if worldPrint or printStates:
+			print 
+			print "Finished after", iterations, "moves."
+		return iterations
+		

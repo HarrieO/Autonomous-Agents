853ab29f156731378947aee67c9f2d6ea3080cda
HarrieO <harrieoosterhuis@gmail.com>
2014-11-10 12:16:49 +0100

    finished world.py

--- Assignment 2/world.py
+++ Assignment 2/world.py
@@ -1,5 +1,4 @@
-from agent import *
-from prey import *
+import random
 
 class World(object):
 	def __init__(self, preyLoc, predatorLoc, width=11, height=11):
@@ -7,6 +6,24 @@ class World(object):
 		self.height 	= height
 		# position is the relative distance between predator and prey
 		self.position  	= self.relativedist(predatorLoc[0]-preyLoc[0],predatorLoc[1]-preyLoc[1])
+
+	# sets state of world to relative position
+	def setState(self, position):
+		self.position = position
+
+	# list of all potential moves
+	def moveList(self):
+		return [(0,0),(0,-1),(0,1),(1,0),(-1,0)]
+
+	# list of all possible starting-states, (not the state where the predator is at the preys location)
+	def allStates(self):
+		return [(dx,dy) for dx in range(-(self.width-1)/2,(self.width-1)/2) \
+		                for dy in range(-(self.height-1)/2,(self.height-1)/2) \
+		                if (dx,dy) != (0,0) ]
+
+	# returns true if predator is at the same location of the prey
+	def stopState(self):
+		return self.position == (0,0)
 	
 	# torialdistance on a dimension given an absolute distance and the span of dimension in the world (width height)
 	def toroidaldis(self, distance, span):
@@ -31,3 +48,25 @@ class World(object):
 	# moves the prey (move is a tuple (dx,dy))
 	def preyMove(self, move):
 		self.position = self.posAfterPreyMove(move)
+
+	# return list of moves the prey can make, and a list of probabilities of each move
+	def preyMoves(self):
+		moveList = [(0,-1),(0,1),(1,0),(-1,0)]
+		for i, move in enumerate(moveList):
+			if self.posAfterPreyMove(move) == (0,0):
+				del moveList[i]
+		probs = [0.2/len(moveList)]*len(moveList)
+		return zip([(0,0)] + moveList, [0.8] + probs)
+
+	# list of states after preymove with each probability
+	def nextPreyStates(self):
+		return [(self.posAfterPreyMove(move), prob) for move, prob in self.preyMoves()]
+
+	# performs a random prey move according to probability distribution of preyMoves
+	def performPreyMove(self):
+		pick = random.random()
+		for state, prob in self.nextPreyStates():
+			if pick <= prob:
+				self.position = state
+				break
+			pick -= prob

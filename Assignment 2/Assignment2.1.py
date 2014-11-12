07d23ba434b6d9d541be482c9f9c89c408d214d3
HarrieO <harrieoosterhuis@gmail.com>
2014-11-12 14:16:29 +0100

    not sure

--- Assignment 2/Assignment2.1.py
+++ Assignment 2/Assignment2.1.py
@@ -3,15 +3,17 @@ import numpy as np
 from pylab import *
 
 runCount = 20
-for alpha in range(0.1,0.6,0.1):
-	aveSteps = np.zeros(1000)
+for alpha in [0.1,0.2,0.3,0.4,0.5]:
+	for discount in [0.1,0.5,0.7,0.9]:
+		
+		aveSteps = np.zeros(1000)
 
-	for i in range(runCount):
-		aveSteps += np.array(Q.Qlearning(1000,Q.epsGreedyPolicy,alpha=alpha))
-	aveSteps /= runCount
+		for i in range(runCount):
+			aveSteps += np.array(Q.Qlearning(1000,Q.epsGreedyPolicy,alpha=alpha,discount=discount))
+		aveSteps /= runCount
 
-	t = range(1, aveSteps.shape[0]+1)
-	plot(t, aveSteps)
+		t = range(1, aveSteps.shape[0]+1)
+		plot(t, aveSteps)
 
 xlabel('episodes')
 ylabel('Average steps per episode')

dbede6a148248bc3a6ef1d8b20b65c1172b85dbd
HarrieO <harrieoosterhuis@gmail.com>
2014-12-09 19:03:30 +0100

    Assignment 3.2.1 code af

--- Assignment 3/Assignment3.1.py
+++ Assignment 3/Assignment3.1.py
@@ -7,24 +7,24 @@ import pylab as pl
 predatorLocations = [(0,0),(0,10),(10,0),(10,10)]
 preds = []
 iters = []
-prey = []
+prey  = []
 for no in range(len(predatorLocations)):
     world = World((5,5),predatorLocations[:no+1])
 
-    allMoves = world.allMoveList()
+    allMoves    = world.allMoveList()
     singleMoves = world.singleMoveList()
 
-    runs = 1000
+    runs            = 1000
     totalCaughtPrey = 0
     totalIterations = 0
     for i in range(runs):
-        world = World((5,5),predatorLocations[:no+1])
+        world      = World((5,5),predatorLocations[:no+1])
         iterations = 0
         while not world.stopState():
-            preyMove = random.choice(singleMoves)
+            preyMove      = random.choice(singleMoves)
             predatorMoves = random.choice(allMoves)
-            reward = world.move(preyMove,predatorMoves)
-            iterations += 1
+            reward        = world.move(preyMove,predatorMoves)
+            iterations   += 1
     
         if reward[0] < 0:
             totalCaughtPrey += 1
--- Assignment 3/Assignment3.2.1.py
+++ Assignment 3/Assignment3.2.1.py
@@ -1,19 +1,99 @@
 from Qlearning import Qlearning
 from policies import epsGreedyPolicy
-import pylab as plt
+import pylab as pl
+import numpy as np
 
-#world = World((5,5),[(10,0),(0,10),(0,0),(10,10)])
-
-predatorLocations = [(0,0)]
-
+ax = pl.subplot(111)
+pl.title('Probability of episode ending with prey caught.')
+pl.ylabel('Prey caught ratio')
+pl.xlabel('Episodes')
+predatorLocations = [(0,0),(10,0),(0,10),(10,10)]
 episodes = 10000
-steps, rewards = Qlearning(episodes, predatorLocations, epsGreedyPolicy)
+runs = 500
+labels = []
+avesteps = np.zeros((4,episodes))
+avestepscatch = np.zeros((4,episodes))
+avestepscrash = np.zeros((4,episodes))
+avecatch = np.zeros((4,episodes))
+for predators in range(1,5)[::-1]:
+    totalcatch = np.zeros((episodes))
+    for i in range(runs):
+        steps, rewards = Qlearning(episodes, predatorLocations[:predators], epsGreedyPolicy)
+        steps   = np.array(steps)
+        rewards = np.array(rewards)
+        avesteps[predators-1,:] += steps
+        avecatch[predators-1,:] += rewards
+        avestepscatch[predators-1,rewards>0] += steps[rewards>0]
+        avestepscrash[predators-1,rewards<=0] += steps[rewards<=0]
+        totalcatch[rewards>0]   += 1
+        print predators, i
 
-for s,r in zip(steps,rewards):
-    if r > 0:
-        print s,"Caught!"
-    else:
-        print s
+    avecatch[predators-1,:] /= runs
+    avesteps[predators-1,:] /= runs
+    avestepscatch[predators-1,:] /= totalcatch
+    if predators > 1:
+        avestepscrash[predators-1,:] /= (runs-totalcatch)
+    pl.plot(range(episodes),avecatch[predators-1,:])
+    labels.append(str(predators) + " predators")
 
-plt.scatter(range(episodes),rewards)
-plt.show()
+
+# Shrink current axis by 20%
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+# Put a legend to the right of the current axis
+ax.legend(labels,loc='center left', bbox_to_anchor=(1, 0.5))
+
+pl.savefig("plots/ratio321.png")
+pl.close()
+
+pl.figure()
+ax = pl.subplot(111)
+pl.title('Average number of iterations per episode.')
+pl.ylabel('Iterations in episode')
+pl.xlabel('Episodes')
+for predators in range(1,5):
+    pl.plot(range(episodes),avesteps[predators-1,:])
+
+# Shrink current axis by 20%
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+# Put a legend to the right of the current axis
+ax.legend(labels,loc='center left', bbox_to_anchor=(1, 0.5))
+pl.savefig("plots/steps321.png")
+
+pl.figure()
+ax = pl.subplot(111)
+pl.title('Average number of iterations per episode ending with prey caught.')
+pl.ylabel('Iterations in episode')
+pl.xlabel('Episodes')
+for predators in range(2,5):
+    ind = np.logical_not(np.isnan(avestepscatch[predators-1,:]))
+    pl.plot(np.arange(episodes)[ind],avestepscatch[predators-1,ind])
+
+# Shrink current axis by 20%
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+# Put a legend to the right of the current axis
+ax.legend(labels[:-1],loc='center left', bbox_to_anchor=(1, 0.5))
+pl.savefig("plots/stepscatch321.png")
+
+
+pl.figure()
+ax = pl.subplot(111)
+pl.title('Average number of iterations per episode ending with prey uncaught.')
+pl.ylabel('Iterations in episode')
+pl.xlabel('Episodes')
+for predators in range(2,5):
+    ind = np.logical_not(np.isnan(avestepscrash[predators-1,:]))
+    pl.plot(np.arange(episodes)[ind],avestepscrash[predators-1,ind])
+
+# Shrink current axis by 20%
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+# Put a legend to the right of the current axis
+ax.legend(labels[:-1],loc='center left', bbox_to_anchor=(1, 0.5))
+pl.savefig("plots/stepscrash321.png")

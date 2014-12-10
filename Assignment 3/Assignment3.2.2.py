d17ca689368fdab96054fb55d07924ec0ce33f22
HarrieO <harrieoosterhuis@gmail.com>
2014-12-10 14:28:33 +0100

    liniear programming

---
+++ Assignment 3/Assignment3.2.2.py
@@ -0,0 +1,7 @@
+import pylab as pl
+import numpy as np
+from world import World
+from minimax import minimax
+
+
+w = World((5,5),[(0,0),(0,10),(10,0),(10,10)])
--- Assignment 3/minimax.py
+++ Assignment 3/minimax.py
@@ -13,17 +13,17 @@ def minimax(episodes,initial_state,epsilon, decay, gamma, alpha=1.0):
     pi_prey = dict()
     initValue = 1.0
     # initialisation
-    # world = World((5,5),initialState)
-    # for state in world.allStates():
-    #   V_pred[state] = 1.0
-    #   V_prey[state] = 1.0
-    #   for action in world.allMoveList():
-    #       pi_pred[(state,action)]=1.0/num_actions
-    #       for opponent_move in opponent_moves:
-    #           Q_pred[(state,action, prey_move)]=1.0
-    #           Q_prey[(state,opponent_move,action)]=1.0
-    #   for action in world.singleMoveList():
-    #       pi_pred[(state,action)]=1.0/num_actions
+    world = World((5,5),initialState)
+    for state in world.allStates():
+      V_pred[state] = 1.0
+      V_prey[state] = 1.0
+      for action in world.allMoveList():
+          pi_pred[(state,action)]=1.0/num_actions
+          for opponent_move in opponent_moves:
+              Q_pred[(state,action, prey_move)]=1.0
+              Q_prey[(state,opponent_move,action)]=1.0
+      for action in world.singleMoveList():
+          pi_pred[(state,action)]=1.0/num_actions
 
     steps = [0]*episodes
     rewards = [0]*episodes
@@ -42,14 +42,36 @@ def minimax(episodes,initial_state,epsilon, decay, gamma, alpha=1.0):
             reward = world.move(action_prey,action_pred)
             iterations +=1
             # update Q
-            if (state,action_prey) not in Q_prey:
-                Q_prey[state,action_prey] = initValue
-            if (state,action_pred) not in Q_pred:
-                Q_pred[state,action_pred] = initValue
+            # if (state,action_prey) not in Q_prey:
+            #     Q_prey[state,action_prey] = initValue
+            # if (state,action_pred) not in Q_pred:
+            #     Q_pred[state,action_pred] = initValue
             Q_pred[(state,action_pred,action_prey)] = (1.0-alpha)*Q_pred[(state,action_pred,action_prey)] + alpha*(reward[1]+ gamma* V_pred[new_state])
             Q_prey[(state,action_prey,action_pred)] = (1.0-alpha)*Q_prey[(state,action_prey,action_pred)] + alpha*(reward[0]+ gamma* V_prey[new_state])
 
             # update pi
+            # adapted from example: http://abel.ee.ucla.edu/cvxopt/examples/tutorial/lp.html
+
+            # constraint to minimize w.r.t. prey action
+            minConstr   = [[1.0] + [Q_pred[(state,a_pred,a_prey)] for a_pred in world.allMoveList()] for a_prey in world.singleMoveList()]
+            # constrinat to keep every pi(a) positive
+            posConstr   = []
+            for i in range(1,len(world.allMoveList())+1):
+                new_row    = [0.0] * (len(world.allMoveList())+1)
+                new_row[i] = -1.0
+                posConstr.append(new_row)
+
+            normGreater = [0.0] + [1.0] * len(world.allMoveList())
+            normSmaller = [0.0] + [-1.0] * len(world.allMoveList())
+
+
+            A = matrix([normGreater, normSmaller] + minConstr + posConstr).trans()
+            b = matrix([ 1.0, -1.0] + [0.0] * (len(world.singleMoveList()) * 2) )
+            # -1 V and 0 for all pi(s,a)
+            c = matrix([ -1.0 ] + [0.0] * len(world.allMoveList()))
+            sol=solvers.lp(c,A,b)
+            print sol
+            exit()
 
             # update V
             #[pi_pred[(s,action_pd)]*Q[s,action_pd, action_py]]
